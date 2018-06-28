44ef8c910c64b20461bf182d8ee89a22cfd61268
Peggy Lee <peggyleesix@gmail.com>
2018-06-28 18:10:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+ def batch_badge_creator(speakers)
+  speakers.map do|speaker|
+      "Hello, my name is #{speaker}."
+     end
+  end
+
+ def assign_rooms
+   speakers.map.each_with_index do |speaker,room|
+   "Hello, #{speakers}! You'll be assigned to room #{room}!"
+  end
+ end
