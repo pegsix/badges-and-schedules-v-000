25368f3aac63917365fd1054f92ea64b8634d484
Peggy Lee <peggyleesix@gmail.com>
2018-06-28 18:20:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,3 +18,7 @@ end
    "Hello, #{speaker}! You'll be assigned to room #{room}!"
   end
  end
+
+ Now you have to tell the printer what to print. Create a method called `printer` that will output first the results of the `batch_badge_creator` method and then of the `assign_rooms` method to the screen.
+   * *Hint*: Remember that methods can call other methods. If the return value of `assign_rooms` is an array of room assignments, how can you print out each assignment? You'll need to iterate over your array of room assignments in order to `puts` out each individual assignment.
+ 
